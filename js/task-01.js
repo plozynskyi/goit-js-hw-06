223a1be9c78b1132c09f8a74c970ee33154c2516
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-10-24 22:20:00 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const elRef = document.querySelectorAll(".item").length;
+console.log(elRef);
+
+const elRef1 = document.querySelectorAll("li.item").length;
+console.log(elRef1);
+
+const elRef2 = document.querySelectorAll(".item").length;
+console.log(elRef2);
+
+const elRef3 = document.querySelectorAll(".item").length;
+console.log(elRef3);
