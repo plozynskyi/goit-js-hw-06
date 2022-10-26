d6f88e538c48d852efc5ddd4f7bf5e6b97b9fdb4
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-10-26 22:04:57 +0300

    fin task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.querySelector("#ingredients");
+for (let i = 0; i < ingredients.length; i++) {
+  let liItem = document.createElement("li");
+  liItem.classList.add("item");
+  liItem.textContent = ingredients[i];
+  listEl.appendChild(liItem);
+  console.log(liItem);
+}
