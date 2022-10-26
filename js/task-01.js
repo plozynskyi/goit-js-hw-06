ce0dd77b770ffad0520cf46357bd697f353d7377
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-10-26 19:32:27 +0300

    fin first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,17 @@
-const elRef = document.querySelectorAll(".item").length;
-console.log(elRef);
+const primaryListRef = document.querySelectorAll("#categories");
+const textElements = document.querySelectorAll("h2");
+const listItem = document.querySelectorAll("li.item");
+const firstListItem = listItem[0].querySelectorAll("li");
+const secondListItem = listItem[1].querySelectorAll("li");
+const thirdListItem = listItem[2].querySelectorAll("li");
 
-const elRef1 = document.querySelectorAll("li.item").length;
-console.log(elRef1);
+console.log(`Number of categories: ${primaryListRef.length}`);
 
-const elRef2 = document.querySelectorAll(".item").length;
-console.log(elRef2);
+console.log(`Category: ${textElements[0].textContent}`);
+console.log(`Elements: ${firstListItem.length}`);
 
-const elRef3 = document.querySelectorAll(".item").length;
-console.log(elRef3);
+console.log(`Category: ${textElements[1].textContent}`);
+console.log(`Elements: ${secondListItem.length}`);
+
+console.log(`Category: ${textElements[2].textContent}`);
+console.log(`Elements: ${thirdListItem.length}`);
