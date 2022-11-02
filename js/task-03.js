7f684bb74db64bba13390f16bb232519c4773a84
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-11-02 23:42:08 +0200

    sum

--- js/task-03.js
+++ js/task-03.js
@@ -1,50 +1,25 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   },
 ];
 
-const galleryListEl = document.querySelector(".gallery");
+const galleryListEl = document.querySelector('.gallery');
 
-// for (const image of images) {
-//   let galleryItems = document.createElement("li");
-//   let galleryLinks = document.createElement("img");
-//   galleryLinks.classList.add("link__img");
-//   galleryLinks.src = image.url;
-//   galleryLinks.alt = image.alt;
-//   galleryLinks.width = 400;
-//   galleryItems.appendChild(galleryLinks);
-//   galleryListEl.appendChild(galleryItems);
-// }
-
-// const elements = images.map((image) => {
-//   let galleryItems = document.createElement("li");
-//   let galleryLinks = document.createElement("img");
-//   galleryLinks.classList.add("link__img");
-//   galleryLinks.src = image.url;
-//   galleryLinks.alt = image.alt;
-//   galleryLinks.width = 400;
-//   galleryItems.appendChild(galleryLinks);
-//   galleryListEl.appendChild(galleryItems);
-// });
-
-const elements = images.map((image) => {
+const elements = images.map(({ url, alt }) => {
   galleryListEl.insertAdjacentHTML(
-    "beforeend",
-    `<li class="gallery__item list"><img class="gallery__img" src="${image.url}" alt="${image.alt}"></li>`
+    'beforeend',
+    `<li class="gallery__item list"><img class="gallery__img" src="${url}" alt="${alt}"></li>`,
   );
 });
 
 console.log(galleryListEl);
-
-const btnBack = document.querySelector("p");
-btnBack.classList.add("link__back");
