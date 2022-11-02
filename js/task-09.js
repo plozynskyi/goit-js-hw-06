275b0cbe48558cf95eab5576e056b1aef7c69179
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-11-02 17:47:45 +0200

    fin #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const changeColorBtn = document.querySelector('.change-color');
+const spanColorText = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor(getRandomHexColor) {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  body.style.backgroundColor = getRandomHexColor();
+  spanColorText.textContent = getRandomHexColor();
 }
